c412ea300356185da16f8e1c309e4207d72eced4
Thijmen Bakker <44574525+tb7512@users.noreply.github.com>
2023-05-08 12:24:30 -0500

    added pwd call

--- main.go
+++ main.go
@@ -78,6 +78,8 @@ func handleInput(w io.Writer, input string, exit chan<- struct{}) error {
 		return builtins.ChangeDirectory(args...)
 	case "env":
 		return builtins.EnvironmentVariables(w, args...)
+	case "pwd":
+		return builtins.pwd(w, args...)
 	case "exit":
 		exit <- struct{}{}
 		return nil
