f7e878cb5c8e4f733cadc67120eecf447d90985d
Mahmoud-Algharbawi <95453406+Mahmoud-Algharbawi@users.noreply.github.com>
2023-05-10 20:23:54 -0500

    updated gistory.go and added more functionalities

--- builtins/history.go
+++ builtins/history.go
@@ -3,7 +3,6 @@ package builtins
 import (
 	"fmt"
 	"io"
-	"os"
 	"strconv"
 )
 
@@ -16,9 +15,69 @@ func AddCommandToHistory(command string) {
 
 // History prints out the command history
 func History(w io.Writer, args ...string) error {
-	// Print out the history slice
-	for i, cmd := range history {
-		fmt.Fprintf(w, "%d %s\n", i+1, cmd)
+	// Parse the options and arguments.
+	var (
+		clear  bool
+		printR bool
+		printH bool
+		n      int
+		err    error
+	)
+	for i := 0; i < len(args); i++ {
+		switch args[i] {
+		case "-c":
+			clear = true
+		case "-r":
+			printR = true
+		case "-h":
+			printH = true
+		default:
+			// Parse the argument as an integer.
+			if n, err = strconv.Atoi(args[i]); err != nil {
+				return fmt.Errorf("invalid argument: %s", args[i])
+			}
+		}
 	}
+
+	// Handle the options.
+	if clear {
+		// Clear the history.
+		history = make([]string, 0)
+	} else if printH {
+		// Print the history without event numbers.
+		for _, cmd := range history {
+			_, _ = fmt.Fprintln(w, cmd)
+		}
+	} else {
+		// Print the history with event numbers.
+		if printR {
+			// Reverse the order of the history.
+			for i := len(history) - 1; i >= 0; i-- {
+				_, _ = fmt.Fprintf(w, "%d %s\n", i+1, history[i])
+			}
+		} else {
+			for i, cmd := range history {
+				_, _ = fmt.Fprintf(w, "%d %s\n", i+1, cmd)
+			}
+		}
+	}
+
+	// Display a specific number of commands.
+	if n > 0 {
+		if n > len(history) {
+			n = len(history)
+		}
+		if printR {
+			// Print the last n commands in reverse order.
+			for i := len(history) - n; i < len(history); i++ {
+				_, _ = fmt.Fprintf(w, "%d %s\n", i+1, history[i])
+			}
+		} else {
+			for i := 0; i < n; i++ {
+				_, _ = fmt.Fprintf(w, "%d %s\n", i+1, history[i])
+			}
+		}
+	}
+
 	return nil
 }
